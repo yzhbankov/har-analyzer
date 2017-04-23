d47f77f56ffaddf4ae8c6c73b966d2bf1e800808
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-04-23 16:10:16 +0300

    view data from har

--- client/component/HttpInfo.js
+++ client/component/HttpInfo.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react'
 
-export default class HttpInfo extends Component{
-    render(){
-        return(
-            <div>HttpInfo</div>
+export default class HttpInfo extends Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-1 HttpInfoTime">{this.props.time}</div>
+                <div className="col-md-2 HttpInfoTitle">{this.props.title}</div>
+                <div className="col-md-1 HttpInfoMethod">{this.props.reqMethod}</div>
+                <div className="col-md-1 HttpInfoStatus">{this.props.resStatus}</div>
+                <div className="col-md-1 HttpInfoReqSize">{(() => {
+                    if (this.props.reqSize <= 0) {
+                        return '-'
+                    } else {
+                        return this.props.reqSize
+                    }
+                })()
+                }</div>
+                <div className="col-md-1 HttpInfoResSize">{
+                    (() => {
+                        if (this.props.resSize <= 0) {
+                            return '-'
+                        } else {
+                            return this.props.resSize
+                        }
+                    })()
+                }
+                </div>
+                <div className="col-md-5 HttpInfoTotalTime">{Math.round(this.props.totalTime*100)/100}</div>
+            </div>
         )
     }
 }
--- client/component/HttpList.js
+++ client/component/HttpList.js
@@ -12,8 +12,25 @@ export default class HttpList extends Component {
         return (
             <div>
                 <div>HttpList</div>
+                <div className="row">
+                    <div className="col-md-1 HttpInfoTime">Time</div>
+                    <div className="col-md-2 HttpInfoTitle">Title</div>
+                    <div className="col-md-1 HttpInfoMethod">Request</div>
+                    <div className="col-md-1 HttpInfoStatus">Req status</div>
+                    <div className="col-md-1 HttpInfoReqSize">Req size</div>
+                    <div className="col-md-1 HttpInfoResSize">Res size</div>
+                    <div className="col-md-5 HttpInfoTotalTime">Time line</div>
+                </div>
                 {this.props.entries.map(entrie =>
-                    <HttpInfo />
+                    <HttpInfo
+                        time={entrie.startedDateTime}
+                        title={entrie.request.url}
+                        reqMethod={entrie.request.method}
+                        resStatus={entrie.response.status}
+                        reqSize={entrie.request.headersSize}
+                        resSize={entrie.response.headersSize + entrie.response.bodySize}
+                        totalTime={entrie.time}
+                    />
                 )}
             </div>
         )
