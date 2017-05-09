b2344537f180cbf2fa77f82df5ea424f01ceb8aa
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-05-09 22:30:10 +0300

    change layout

--- client/component/Statistics.js
+++ client/component/Statistics.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Radium from 'radium'
-import {defaultButton, primaryButton, successButton, regularTitle, chapterTitle, center} from '../style/components'
+import {defaultButton, primaryButton, successButton, regularTitle, chapterTitle, left} from '../style/components'
+import {blockSpacing} from '../style/style'
 
 import StatisticsResponseContent from './Statistics/StatisticsResponseContent'
 import StatisticsTiming from './Statistics/StatisticsTiming'
@@ -33,30 +34,41 @@ export default class Statistics extends Component {
     }
 
     render() {
-        return (
-            <div style={center}>
-                <button style={defaultButton} onClick={this.showHideStatistics.bind(this)}>Show statistics</button>
-                {!this.state.show || this.props.statistics.map((statistic, number) =>
-                    <div>
-                        <button style={primaryButton}
-                            onClick={this.showHidePageStatistics.bind(this, number)}>{this.props.pages[number].title}</button>
-                        {!this.state.showPage[number] || <div className="row">
-                            <div style={chapterTitle}>{this.props.pages[number].title}</div>
-                            <div className="col-md-4" style={regularTitle}>
-                                <div>Response content statistics</div>
-                                <StatisticsResponseContent data={statistic.responseContent} number={number}/>
-                            </div>
-                            <div className="col-md-4" style={regularTitle}>
-                                <div>Time statistics</div>
-                                <StatisticsTiming data={statistic.timing} number={number}/></div>
-                            <div className="col-md-4" style={regularTitle}>
-                                <div>Head/body statistics</div>
-                                <StatisticsHeaderBodySendReceive data={statistic.headBody} number={number}/>
-                            </div>
-                        </div>}
-                    </div>)
+        return (<div style={left}>
+                <button style={[defaultButton, {position: 'absolute', top: '1px', left: '150px'}]}
+                        onClick={this.showHideStatistics.bind(this)}>Show/hide statistics
+                </button>
+                {!this.state.show || <div style={chapterTitle}>STATISTICS</div>}
+                {!this.state.show || this.props.statistics.map((statistic, number) => {
+                    let color = 'white';
+                    if (number % 2 != 0) {
+                        color = '#f8f8ff';
+                    }
+                    const backgroundStyle = {backgroundColor: color};
+                    return (
+                        <div>
+                            <hr style={{margin: 0}}/>
+                            <a href="#" style={{textDecoration:'none'}}><div style={[chapterTitle, left, backgroundStyle]}
+                                 onClick={this.showHidePageStatistics.bind(this, number)}>{this.props.pages[number].title}</div>
+                            </a>
+                            <hr style={{margin: 0}}/>
+                            {!this.state.showPage[number] || <div className="row" style={{margin:0}}>
+                                <div style={chapterTitle}>{this.props.pages[number].title}</div>
+                                <div className="col-md-4" style={regularTitle}>
+                                    <div>Response content statistics</div>
+                                    <StatisticsResponseContent data={statistic.responseContent} number={number}/>
+                                </div>
+                                <div className="col-md-4" style={regularTitle}>
+                                    <div>Time statistics</div>
+                                    <StatisticsTiming data={statistic.timing} number={number}/></div>
+                                <div className="col-md-4" style={regularTitle}>
+                                    <div>Head/body statistics</div>
+                                    <StatisticsHeaderBodySendReceive data={statistic.headBody} number={number}/>
+                                </div>
+                            </div>}
+                        </div>)
+                })
                 }
-
             </div>
         )
     }
--- client/component/Title.js
+++ client/component/Title.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react'
 import Radium from 'radium'
-import {primaryTitle, chapterTitle, defaultButton, primaryButton, successButton, dangerButton, medeumBlock, center} from '../style/components.js'
+import {
+    primaryTitle,
+    chapterTitle,
+    defaultButton,
+    primaryButton,
+    successButton,
+    dangerButton,
+    center
+} from '../style/components.js'
+import {textColor, blockSpacing} from '../style/style'
 
 @Radium
 export default
@@ -26,21 +35,21 @@ class Title extends Component {
             opacity: 0,
             filter: 'alpha(opacity=0)'
         };
-        return (<div>
-                <div style={primaryTitle}>HAR Analyser</div>
-                <div className="row">
-                    <div className="col-md-10">
-                        <div className="btn btn-primary" style={[fileUpload, primaryButton]}>
+        return (
+                <div className="row" style={{backgroundColor:'grey', overflow:'hidden', margin:'0'}}>
+                    <div className="col-md-1">
+                        <div style={[fileUpload, defaultButton]}>
                             <span>Load HAR File</span>
                             <input type="file" style={upload} onChange={this.readFile.bind(this)}/>
                         </div>
                     </div>
-                    <div className="col-md-2"><a href="discuss.html" style={{textDecoration:'none'}}>
-                        <div style={[successButton, medeumBlock, center]}>Let`s discuss</div>
+                    <div className="col-md-1"></div>
+                    <div className="col-md-8" style={[primaryTitle, center, textColor.white, blockSpacing.one]}>HAR VIEWER</div>
+                    <div className="col-md-2"><a href="discuss.html" style={{textDecoration: 'none'}}>
+                        <div style={[blockSpacing.five, chapterTitle, center, textColor.white]}>Let`s discuss</div>
                     </a>
                     </div>
                 </div>
-            </div>
         )
     }
 }
