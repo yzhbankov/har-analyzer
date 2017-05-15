db5ca2db7b0b793d586bf11cde4e849fad8bab05
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-05-15 21:36:46 +0300

    make responsive layout

--- client/component/HttpInfoDetails/HttpInfoDetailsResponse.js
+++ client/component/HttpInfoDetails/HttpInfoDetailsResponse.js
@@ -4,41 +4,38 @@ export default class HttpInfoDetailsResponse extends Component {
     render() {
         return (
             <div className='column is-12 box content is-small'>
-                <div className='columns'>
+                <div className='columns is-mobile'>
                     <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>Status</div>
                     <div
-                        className='column is-9 is-vertical-spaceless'>{this.props.response.status} {this.props.response.statusText}.
+                        className='column is-9 is-vertical-spaceless box-content'>{this.props.response.status} {this.props.response.statusText}.
                         {this.props.response.httpVersion}</div>
                 </div>
-                <div className="columns">
-                    <div className="column is-3 has-text-bold has-text-left is-vertical-spaceless">Response headers
+                <div className="columns is-mobile">
+                    <div className="column is-12 has-text-bold has-text-left is-vertical-spaceless">Response headers
                     </div>
                 </div>
-                <hr/>
                 {
                     this.props.response.headers.map((header) => {
                         return (
-                            <div className='columns'>
-                                <div
-                                    className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>{header.name}</div>
-                                <div className='column is-9 is-vertical-spaceless'>{header.value}</div>
+                            <div className='columns is-mobile'>
+                                <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>{header.name}</div>
+                                <div className='column is-9 is-vertical-spaceless box-content'>{header.value}</div>
                             </div>
                         )
                     })
                 }
-                <div className="columns">
+                <div className="columns is-mobile">
                     <div className="column is-3 has-text-bold is-vertical-spaceless has-text-left">Size</div>
                 </div>
-                <hr/>
-                <div className='columns'>
+                <div className='columns is-mobile'>
                     <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>Body size</div>
                     <div className='column is-9 is-vertical-spaceless'>{this.props.response.bodySize} B</div>
                 </div>
-                <div className='columns'>
+                <div className='columns is-mobile'>
                     <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>Header size</div>
                     <div className='column is-9 is-vertical-spaceless'>{this.props.response.headersSize} B</div>
                 </div>
-                <div className='columns'>
+                <div className='columns is-mobile'>
                     <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>Total</div>
                     <div
                         className='column is-9 is-vertical-spaceless'>{this.props.response.bodySize + this.props.response.headersSize}
--- client/component/HttpInfoDetails/HttpInfoDetailsResponseContent.js
+++ client/component/HttpInfoDetails/HttpInfoDetailsResponseContent.js
@@ -4,15 +4,15 @@ export default class HttpInfoDetailsResponseContent extends Component {
     render() {
         return (
             <div className='column is-12 box content is-small'>
-                <div className='columns'>
+                <div className='columns is-mobile'>
                     <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>Size</div>
                     <div className='column is-3 is-vertical-spaceless'>{this.props.content.size} B</div>
                 </div>
-                <div className='columns'>
+                <div className='columns is-mobile'>
                     <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>MIME Type</div>
                     <div className='column is-3 is-vertical-spaceless'>{this.props.content.mimeType}</div>
                 </div>
-                <div className='columns'>
+                <div className='columns is-mobile'>
                     <div className='column is-3 has-text-right is-vertical-spaceless has-text-bold'>Response content</div>
                     <div className='column is-9 is-vertical-spaceless box-content'>{this.props.content.text}</div>
                 </div>
