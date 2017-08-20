830bb84ed7421dcec6459beaf797a27b43826bd7
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-08-20 20:53:53 +0300

    fix modal rendering

--- client/component/modals/LinkModal.js
+++ client/component/modals/LinkModal.js
@@ -5,14 +5,15 @@ import React, {Component} from 'react'
 
 export default class LinkModal extends Component {
     constructor(props) {
-        super();
-        this.sate = {
+        super(props);
+        this.state = {
             modal: "modal"
         }
     }
-    componentWillReceiveProps(nextProps){
+
+    componentWillReceiveProps(nextProps) {
         console.log(nextProps);
-        if (nextProps.generatedLink){
+        if (nextProps.generatedLink) {
             this.setState({
                 modal: "modal is-active"
             })
@@ -27,18 +28,18 @@ export default class LinkModal extends Component {
 
     render() {
         return (
-            <div className={this.sate.modal}>
+            <div className={this.state.modal}>
                 <div className="modal-background"></div>
                 <div className="modal-card">
                     <header className="modal-card-head">
-                        <p className="modal-card-title">Modal title</p>
+                        <p className="modal-card-title">Generated link</p>
                         <button className="delete" aria-label="close"></button>
                     </header>
                     <section className="modal-card-body">
                         http://localhost:8080/hars/{this.props.generatedLink}
                     </section>
                     <footer className="modal-card-foot">
-                        <button className="button is-success">Save changes</button>
+                        <button className="button is-success">Copy link</button>
                         <button onClick={this.closeModal} className="button">Close</button>
                     </footer>
                 </div>
